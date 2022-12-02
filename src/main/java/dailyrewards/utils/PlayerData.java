3eb3a4579ad564a047f79a9f68e759726e0cfd03
iChocoMC <99765099+iChocoMC@users.noreply.github.com>
2022-12-01 22:07:28 -0300

    Update PlayerData.java

--- src/main/java/dailyrewards/utils/PlayerData.java
+++ src/main/java/dailyrewards/utils/PlayerData.java
@@ -26,6 +26,13 @@ public class PlayerData implements Runnable {
         }
     }
 
+    /*
+     * Si el jugador ha entrado por primera vez se le pondra como dia 1
+     * En cambio si lleva más de 1 día y entra de vuelta, por ej 2.
+     * Se cambiara ese 2 a 2000. Si el día es mayor a 1000, se podra entender
+     * que el jugador ha entrado, en cambio, si este no entra, el dia sera 2.
+     */
+
     public static void addPlayer(String uuid){
         int day = dataConfig.getInt(uuid);
 
@@ -39,6 +46,10 @@ public class PlayerData implements Runnable {
         }
     }
 
+    /*
+     * Si el jugador ha entrado al servidor por más de 1 día, el día sera mayor a 1000
+     * y si esto es verdad se devolvera el día dividido 1000 para no causar errores
+     */
     public static int getTime(String uuid){
         int day = dataConfig.getInt(uuid);
         if(day > 1000){
@@ -47,6 +58,14 @@ public class PlayerData implements Runnable {
         return day;
     }
 
+    /*
+     * Revisa todas las lineas y si el día del jugador es mayor a 1000
+     * Se añadira 1 día más por entrar al servidor y el día pasara a day/1000.
+     * 
+     * En cambio, si el dia es menor a 1000, se entendera como que ese jugador
+     * no ha entrado, entonces se le restara 1 día. Y si ese día es menor a 0, se borrará
+     * toda información del data.yml
+     */
     @Override
     public void run() {
 
